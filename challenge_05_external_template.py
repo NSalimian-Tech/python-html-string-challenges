2d6c046dbd5801574e08b55ba4c76fbbc28a91fc
Najme Salimian <n1991.salimian@gmail.com>
2026-03-02 15:23:11 -0700

    chore: finally remove incorrect files

--- challenge_05_external_template.py
+++
@@ -1,8 +0,0 @@
-with open("template.html", "r") as file:
-    html_template = file.read()
-
-new_content = "<h2>This content came from Python!</h2>"
-
-final_html = html_template.replace("", new_content)
-
-print(final_html)
